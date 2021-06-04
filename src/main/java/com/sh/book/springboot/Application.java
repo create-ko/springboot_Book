3da5cdbcb14286ef7af53d64dcebca05fad08d0c
create-ko <create_ko@naver.com>
2021-06-04 17:35:08 +0900

    chapter 2 finished.

---
+++ src/main/java/com/sh/book/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.sh.book.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication   // 스프링 부트의 자동설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정하는 어노테이션. 특히 어노테이션이 있는 위치부터 설정을 읽기 때문에 항상 프로젝트의 최상단에 있어야 한다.
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // run 은 내장was를 실행하는 함수
+    }
+}
---
+++ src/test/java/com/sh/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,52 @@
+package com.sh.book.springboot.web;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+        /**
+         * mvc.perform : MockMvc를 통해 http Get 요청을 한다.
+         * .andExpect : 결과를 검증한다.
+         * Status는 응답 코드를 검증하는것(200인지 아닌지를 검증한다)
+         * Content는 응답 본문의 내용을 검증한다.
+         * **/
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        /**
+         * param : API 테스트 할 때 사용될 파라미터를 설정한다. String 값만 허용한다.
+         * jasonPath : JSON 응답값을 필드별로 검증할 수 있는 메소드, $를 기준으로 필드명을 명시.
+         * **/
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+    }
+
+}
