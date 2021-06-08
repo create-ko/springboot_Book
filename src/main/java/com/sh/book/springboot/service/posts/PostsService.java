580363cb3948c62cd6406487b361503b6cbafd24
create-ko <create_ko@naver.com>
2021-06-08 17:41:15 +0900

    chapter 4 finish

--- src/main/java/com/sh/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sh/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.sh.book.springboot.service.posts;
 
 import com.sh.book.springboot.domain.posts.Posts;
 import com.sh.book.springboot.domain.posts.PostsRepository;
+import com.sh.book.springboot.web.dto.PostsListResponseDto;
 import com.sh.book.springboot.web.dto.PostsResponseDto;
 import com.sh.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sh.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,4 +38,17 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        postsRepository.delete(posts);
+    }
 }
