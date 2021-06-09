ef293dba80119d97d34d7906050b505015567633
create-ko <create_ko@naver.com>
2021-06-09 18:49:37 +0900

    chapter 5 first commit;

---
+++ src/main/java/com/sh/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.sh.book.springboot.config.auth;
+
+import com.sh.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+        /**
+         *  csrf().disable().headers().frameOptions().disable() : h2-console 화면을 사용하기 위해 해당 옵션들을 disable한다.
+         *  authorizeRequests : URL별 권환 관리를 설정하는 옵션의 시작점. 선언되어있어야만 antMatchers를 사용할 수 있다.
+         *  anyRequest.authenticated() : 설정된 값들 이외의 URL들은 인증된 사용자들에게만 허용하게 한다.
+         *
+         * **/
+
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+
+
+    }
+
+}
